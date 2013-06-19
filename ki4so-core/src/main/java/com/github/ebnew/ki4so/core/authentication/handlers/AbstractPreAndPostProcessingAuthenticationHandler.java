23779960ba70df858b49705fcaadbe122dec0e56
ywbrj042 <ywbrj042@qq.com>
2013-06-19 09:59:32 +0800

    增加了认证处理器的实现类。

---
+++ ki4so-core/src/main/java/com/github/ebnew/ki4so/core/authentication/handlers/AbstractPreAndPostProcessingAuthenticationHandler.java
@@ -0,0 +1,61 @@
+package com.github.ebnew.ki4so.core.authentication.handlers;
+
+import com.github.ebnew.ki4so.core.authentication.Credential;
+import com.github.ebnew.ki4so.core.exception.InvalidCredentialException;
+
+/**
+ * 抽象的认证处理器实现，提供了具体实现类可以在认证之前和认证之后执行一些任务。
+ * 
+ * @author burgess yang
+ * 
+ */
+public abstract class AbstractPreAndPostProcessingAuthenticationHandler
+		implements AuthenticationHandler {
+	/**
+	 * Method to execute before authentication occurs.
+	 * 
+	 * @param credentials
+	 *            the Credentials supplied
+	 * @return true if authentication should continue, false otherwise.
+	 */
+	protected boolean preAuthenticate(final Credential credential) {
+		return true;
+	}
+
+	/**
+	 * Method to execute after authentication occurs.
+	 * 
+	 * @param credentials
+	 *            the supplied credentials
+	 * @param authenticated
+	 *            the result of the authentication attempt.
+	 * @return true if the handler should return true, false otherwise.
+	 */
+	protected boolean postAuthenticate(final Credential credential,
+			final boolean authenticated) {
+		return authenticated;
+	}
+
+	public final boolean authenticate(final Credential credential)
+			throws InvalidCredentialException {
+
+		if (!preAuthenticate(credential)) {
+			return false;
+		}
+
+		final boolean authenticated = doAuthentication(credential);
+
+		return postAuthenticate(credential, authenticated);
+	}
+
+	
+	/**
+	 * 执行真正的认证方法。
+	 * @param credential 用户凭据。
+	 * @return 认证结果。
+	 * @throws InvalidCredentialException
+	 */
+	protected abstract boolean doAuthentication(final Credential credential)
+			throws InvalidCredentialException;
+
+}
---
+++ ki4so-core/src/main/java/com/github/ebnew/ki4so/core/authentication/handlers/SimpleTestUsernamePasswordAuthenticationHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.ebnew.ki4so.core.authentication.handlers;
+
+import org.springframework.util.StringUtils;
+
+import com.github.ebnew.ki4so.core.authentication.UsernamePasswordCredential;
+
+/**
+ * Simple test implementation of a AuthenticationHandler that returns true if
+ * the username and password match. This class should never be enabled in a
+ * production environment and is only designed to facilitate unit testing and
+ * load testing.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 3.0
+ */
+public final class SimpleTestUsernamePasswordAuthenticationHandler extends
+		AbstractUsernamePasswordAuthenticationHandler {
+
+	public SimpleTestUsernamePasswordAuthenticationHandler() {
+
+	}
+
+	@Override
+	public boolean authenticateUsernamePasswordInternal(
+			final UsernamePasswordCredential credential) {
+		final String username = credential.getUsername();
+		final String password = credential.getPassword();
+
+		if (StringUtils.hasText(username) && StringUtils.hasText(password)
+				&& username.equals(getPasswordEncoder().encode(password))) {
+			return true;
+		}
+		return false;
+	}
+}
