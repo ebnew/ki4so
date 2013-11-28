5de5e120067ab0d11f8ea91c3b486f3e7ce53cde
ywbrj042 <ywbrj042@qq.com>
2013-11-28 14:58:16 +0800

    增加了StringUtils类，去除了对于spring的依赖。

---
+++ ki4so-common/src/main/java/com/github/ebnew/ki4so/common/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package com.github.ebnew.ki4so.common.utils;
+
+public class StringUtils {
+	
+	/**
+	 * 判断字符串是否为空。
+	 * @param str 源字符串。
+	 * @return
+	 */
+	public static boolean isEmpty(String str){
+		return str == null || str.length()==0;
+	}
+
+}
